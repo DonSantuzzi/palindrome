2acf2d8ececcb7558a7c7d874593cbc52f05f19e
Andre Grunow <andre.grunow@1und1.de>
2018-03-27 16:23:44 +0200

    first commit

---
+++ src/main/java/com/unitedinternet/recruit/exercise/PalindromeMain.java
@@ -0,0 +1,32 @@
+package com.unitedinternet.recruit.exercise;
+
+
+
+/**
+ * Write a program that outputs (STDOUT) numeric palindromes as a plain text list separated by a linefeed.
+ *
+ * A palindrome is something that when read from the left is the same as when read from the right.
+ *
+ * Examples for numeric palindromes are : 1, .., 88, 99, 101, 111, 121, ..., 25552, ....
+ *
+ * Output should start at 1. The upper limit for possible palindromes to check is passed in as a command line arg.
+ *
+ * With an upper limit of 1,000, the expected example output is:
+ * 1
+ * 2
+ * ...
+ * 989
+ * 999
+ *
+ * That means you print all numerical palindromes between 1 and 1,000.
+ */
+public final class PalindromeMain {
+
+
+    public static void main(String[] args)  {
+
+    }
+
+}
+
+
